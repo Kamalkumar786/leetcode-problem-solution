f5c9f99078f886bfd40ba37a3559426883693808
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2023-06-05 19:28:36 +0530

    Time: 0 ms (100%), Space: 5.9 MB (87.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // int girds = m+n-2;
-        double paths=1;
 
-        for(int i=1; i<=m-1; i++)
-            paths = paths*(n-1+i)/i;
+        double ans=1;
+
+        for(int i=1; i<min(m,n); i++)
+            ans = ans*(max(m,n)-1+i)/i;
         
-        return (int)paths;
+        return (int)ans;
         
     }
 };
