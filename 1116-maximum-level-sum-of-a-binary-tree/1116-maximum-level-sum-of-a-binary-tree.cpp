2e257021f3ffba874c6cc32412402c30a9b7b67f
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2023-06-15 10:04:04 +0530

    Time: 208 ms (51.38%), Space: 107.3 MB (59.25%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.cpp
@@ -12,26 +12,26 @@
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        int sum = INT_MIN, ans, l = 1;
+        int sum = INT_MIN, ans, c = 1;
         queue<TreeNode*> q;
         q.push(root);
         while(q.size())
          {
-            int n = q.size(), s = 0;
-            while(n)
-             {
-                TreeNode* temp = q.front(); q.pop();
-                s += temp -> val;
-                if(temp -> left) q.push(temp -> left);
-                if(temp -> right) q.push(temp -> right);
-                n--;
+             int s=0;
+            int n=q.size();
+            while(n--){
+                TreeNode* t=q.front();
+                 q.pop();
+                 s+=t->val;
+                if(t->left)q.push(t->left);
+                if(t->right)q.push(t->right);
             }
-            if(s > sum) {
-            sum = s;
-            ans = l;
+            if(s>sum){
+                sum=s;
+                ans=c;
+                }
+                c++;
             }
-            l++;
-        }
-        return ans;
+            return ans;
     }
 };
