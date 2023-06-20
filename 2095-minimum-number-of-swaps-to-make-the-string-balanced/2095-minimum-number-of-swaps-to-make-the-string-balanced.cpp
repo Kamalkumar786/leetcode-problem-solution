5c08b5bc177cda0e35b2d38d68835ae8e03b19d2
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2023-06-20 19:12:25 +0530

    Time: 148 ms (32.35%), Space: 33.6 MB (43.75%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+       stack<char> stk;
+
+    for (char c : s)
+    {
+        if (c == '[')
+            stk.push(c);
+        if (c == ']' && stk.size() != 0)
+            stk.pop();
+    }
+
+    return (stk.size() + 1) / 2; 
+    }
+};
