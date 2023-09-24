55a4850331a284a1bc9ba2fce6c9879b38ae7d56
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2023-09-24 10:34:01 +0530

    Time: 120 ms (77.62%), Space: 99 MB (28.71%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-         int totalSum = 0;
-        for (int num : nums)
-            totalSum += num;
-        int maxLength = -1, currSum = 0;
-        for (int l = 0, r = 0; r < nums.size(); r++) {
-            currSum += nums[r];
-            while (l <= r && currSum > totalSum - x)
-                currSum -= nums[l++];
-            if (currSum == totalSum - x)
-                maxLength = max(maxLength, r - l + 1);
+        int n = nums.size();
+
+        //xcomplement is sum of all elements minus x
+        //we are going to find the longest subarray that adds up to xcomplement
+        //minOperations = n - the longest subarray
+        int xcomplement = -x;
+        for(int num : nums) {
+            xcomplement += num;
         }
-        return maxLength == -1 ? -1 : nums.size() - maxLength;
+        
+        if(xcomplement < 0) {
+            return -1;
+        } else if(xcomplement == 0) {
+            return n;
+        }
+        
+        int i1 = 0, i2 = 0;
+        int sum = 0;
+        int maxWin = 0;
+        while(i2 < n) {
+            sum += nums[i2++]; //window expands on the right side always
+            while(sum > xcomplement) { //shrink the window on the left side to reduce the sum
+                sum -= nums[i1];
+                i1++;
+            }
+            if(sum == xcomplement) {
+                maxWin = max(maxWin, i2 - i1);
+            }
+        }
+        return (maxWin == 0) ? -1 : n - maxWin;
     }
 };
