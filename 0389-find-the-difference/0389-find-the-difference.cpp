f0d2b4504a5a4acbbb50c0c73773859d4fbbfd71
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2023-09-25 23:44:10 +0530

    Time: 12 ms (5.95%), Space: 7.2 MB (8.68%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
       int s1=0,s2=0;
+      s2=t[s.size()];
     for(int i=0;i<s.size();i++){
       s1+=(s[i]);
-      cout<<s1<<endl;
-    }
-     for(int i=0;i<t.size();i++){
       s2+=(t[i]);
+      cout<<s1<<endl;
     }
    return char(abs(s2-s1));
     }
