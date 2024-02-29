2f9b2c70a7efe04bd31d12b51ae1c48d08ec69a5
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2024-03-01 02:41:49 +0530

    Time: 3 ms (84.19%) | Memory: 12.3 MB (73.85%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution{
 public:
- vector < vector < int >> combinationSum(vector < int > & candidates, int target) {
-      vector < vector < int >> ans;
-      vector < int > ds;
-      fun(0, target, candidates, ans, ds);
-      return ans;
-    }
+ vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+     vector<vector<int>>v;
+     vector<int>t;
+ fun(0,target,candidates,v,t);
+ return v;
+ 
+ }
  private:
- void fun(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
-      if (ind == arr.size()) {
-        if (target == 0) {
-          ans.push_back(ds);
+  void fun(int i, int a,vector<int>& c,vector<vector<int>>& v,vector<int>& t){
+        if(i==c.size()){
+        if(a==0){
+           v.push_back(t);
         }
         return;
-      }
-      if (arr[ind] <= target) {
-        ds.push_back(arr[ind]);
-        fun(ind, target - arr[ind], arr, ans, ds);
-        ds.pop_back();
-      }
-
-      fun(ind + 1, target, arr, ans, ds);
-
+        }
+        if(c[i]<=a){
+        t.push_back(c[i]);
+        fun(i,a-c[i],c,v,t);
+        t.pop_back();
+        }
+        fun(i+1,a,c,v,t);
     }
 };
 
