4612240ad1caeb6066a129110935bbee7da2b71d
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2024-03-09 15:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (59.61%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,30 @@
 class Solution {
-private:
-void fun(vector<int>& nums,vector<int>&t,vector<vector<int>>&ans){
-    if(nums.size()==0){
-        ans.push_back(t);
-        return;
-    }
-    for(int i = 0; i < nums.size(); i++) {
-        t.push_back(nums[i]);
-        vector<int> nums2;
-        for(int j = 0; j < i; j++) nums2.push_back(nums[j]);
-        for(int j = i+1; j < nums.size(); j++) nums2.push_back(nums[j]);  
-        fun(nums2, t, ans);
-        t.pop_back();
-         }
-
-}
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
+        vector<vector<int>>v;
+        vector<int>visited(nums.size(),0);
         vector<int>t;
-        int n=nums.size();
-        fun(nums,t,ans);
-        return ans;
+        fun(nums,t,visited,v);
+        return v;
     }
+    private:
+    void fun(vector<int>&nums,vector<int>&t,vector<int>&visited,vector<vector<int>>&v){
+        if(t.size()==nums.size()){
+            v.push_back(t);
+            return ;
+        }
+    
+        for(int i=0;i<nums.size();i++){
+            // cout<<t[i];
+            if(visited[i]==0){
+                t.push_back(nums[i]);
+                visited[i]=1;
+                fun(nums,t,visited,v);
+                visited[i]=0;
+                t.pop_back();
+            }
+
+        }
+    }
+
 };
