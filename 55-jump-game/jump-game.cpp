d19548d36d54c8c715fdfee373f2e977102e931a
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2024-04-26 04:54:50 +0530

    Time: 659 ms (7.85%) | Memory: 54.1 MB (6.24%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool fun(int ind, vector<int>& nums,vector<int>&dp){
+        if(ind==nums.size()-1) return true;
+        if(ind>=nums.size()-1) return false;
+        if(dp[ind]!=-1)return dp[ind];
+        for(int i=1;i<=nums[ind];i++){
+            if(fun(ind+i,nums,dp)) return dp[ind]=true;
+        }
+        return dp[ind]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return fun(0, nums,dp);
+    }
+};
