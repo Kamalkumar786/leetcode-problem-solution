4a53b7be14063e95cfb96b1373db802258b2000b
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2024-04-30 18:59:51 +0530

    Time: 3 ms (73.96%) | Memory: 11.1 MB (74.26%) - LeetSync

---
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  bool isCovered(vector<vector<int>>& ranges, int left, int right) {
+	for(int i = left; i <= right; i++) {
+		bool f = false;
+		for(int j = 0; j < ranges.size() && !f; j++) 
+			if(i >= ranges[j][0] && i <= ranges[j][1]) 
+				f = true;
+		if(!f) return false;
+	}
+	return true;
+}
+};
